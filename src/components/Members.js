e6faba80aa5bd9e38b8e263d1b636186c8e53e89
nuharahman9 <nrahm009@ucr.edu>
2022-08-31 19:20:54 -0700

    dropdown - very buggy rn, working on filtering member classes

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,13 +1,22 @@
 import { useEffect, useState } from 'react';
-import Dropdown from './Dropdown';
 import LoadMembers from '../firebase/LoadMembers'
 import classes from '../style/members.module.css'
 
 
 const Members = () => {
-    const { docs }  = LoadMembers('');
     const [render, setRender] = useState(false); 
+    const [memberSelect, selectMember] = useState('sigma'); 
+    const { docs }  = LoadMembers(memberSelect);
     console.log(docs); 
+
+    function handleSubmit(e) {
+        selectMember(e.target.value);
+    }
+
+    function handleChange(e) {
+        selectMember(e.target.value);
+        console.log(memberSelect); 
+    }
     
      useEffect(() => {
         if (!render){
@@ -18,7 +27,30 @@ const Members = () => {
     return (
         <div> 
         <p>Members</p>
-        <Dropdown /> 
+        <form onSubmit={handleSubmit}>
+          <label>
+            <select value={memberSelect} onChange={handleChange}>
+              <option value="alpha">Alpha</option>
+              <option value="beta">Beta</option>
+              <option value="gamma">Gamma</option>
+              <option value="delta">Delta</option>
+              <option value="zeta">Zeta</option>
+              <option value="eta">Eta</option>
+              <option value="theta">Theta</option>
+              <option value="iota">Iota</option>
+              <option value="kappa">Kappa</option>
+              <option value="lambda">Lamda</option>
+              <option value="mu">Mu</option>
+              <option value="nu">Nu</option>
+              <option value="xi">Xi</option>
+              <option value="omicron">Omicron</option>
+              <option value="pi">Pi</option>
+              <option value="rho">Rho</option>
+              <option value="sigma">Sigma</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
 
         <div className={classes.memberlist}>
             {docs && docs.map(doc => (
--- src/firebase/LoadMembers.js
+++ src/firebase/LoadMembers.js
@@ -1,31 +1,13 @@
 import firestore from './config'; 
-import { collection, query, onSnapshot } from 'firebase/firestore'; 
+import { collection, query, onSnapshot, where } from 'firebase/firestore'; 
 import { useState, useEffect } from 'react';
 
 
 const LoadMembers = (filter) => {
   const [docs, setDocs] = useState([]);
 
-
-  //   async function getData() { 
-  //   const membersRef = collection(firestore, 'members'); 
-  //   const members = 
-    
-    
-  //   //await getDocs(membersRef).catch(err => {
-  //   //    console.log(err);
-  //  // }); 
-  //   let documents = []; 
-  //   members.forEach((doc) => {
-  //       documents.push({...doc.data(), id: doc.id}); 
-  //   }); 
-
-  //   setDocs(documents);
-  //  } 
-
-
  useEffect(() => {
-    const q = query(collection(firestore, 'members')); 
+  const q = query(collection(firestore, 'members'), where(filter, "==", true));
     const unsub = onSnapshot(q, (querySnapshot) => {
       let docs = []; 
       querySnapshot.forEach((doc) => {
@@ -35,6 +17,7 @@ const LoadMembers = (filter) => {
 
     }); 
     return () => unsub(); 
+    
 
  }, [filter]); 
   
