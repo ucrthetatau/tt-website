b88ee88542decb8de228d44c0dfad56284ead50c
jerry-nagata <jared.nagata@gmail.com>
2023-06-26 21:42:35 -0700

    rush page is done, about page is almost done

--- src/components/Rush.js
+++ src/components/Rush.js
@@ -1,62 +1,38 @@
 import React from 'react'
-import styles from '../styles/rush.module.css'
 import s23 from '../static/S23graphic1.jpg'
 import '../styles/rush.css'
 
 function Rush() {
     return (
-        // <div className={styles.container}>
-        //     <h1 className={styles.head}>spring recruitment 2023</h1>
-
-        //     <div className={styles.content}>
-        //         {/* <img src={s23} className={styles.pic} /> */}
-        //         <img src={s23} class={"pic"} />
-        //         <div className={styles.body}>
-        //             <h2> ​Are you an engineer? <br /> Then come rush for Theta Tau! </h2>
-        //             We're the only Co-ed Professional Engineering Fraternity at UCR. Theta Tau can help you in expanding your professional network, becoming a part of a brotherhood, giving back to the community, and becoming a better engineer.
-        //             <br />
-        //             Come and find out what we're all about! We are looking for motivated and bright individuals to become part of our Sigma Class!
-        //             <br />
-        //             Please fill out the interest form if you'd like to join us for our Fall Rush 2023!
-        //         </div>
-        //     </div>
-        //     <button className={styles.button}> Interest Form </button>
-        // </div>
-
-
-        // <div class="rush-container">
-        //     <h1>HEADER</h1>
-        //     <p>Are you an engineer? Then come rush for Theta Tau! We're the only Co-ed Professional Engineering Fraternity at UCR. Theta Tau can help you in expanding your professional network, becoming a part of a brotherhood, giving back to the community, and becoming a better engineer.
-        //     <br></br> <br></br>
-        //     Come and find out what we're all about! We are looking for motivated and bright individuals to become part of our Upsilon Class!
-        //     <br></br> <br></br>
-        //     Please fill out the interest form if you'd like to join us for our Spring Rush 2023!
-        //     </p>
-        //     <div class="buttonContainer">
-        //         <a href="https://forms.gle/K8tom1dVxWma7MA77" target="_blank" rel="noopener noreferrer">
-        //             <button>Interest Form</button>
-        //         </a>
-        //     </div>
-        // </div>
-        // <div class="pic">
-        //     <img src={s23} class="pic"></img>
-        // </div>
         <>
         <div class = "rush-container">
-            <p>
-            Are you an engineer? Then come rush for Theta Tau! We're the only Co-ed Professional Engineering Fraternity at UCR. Theta Tau can help you in expanding your professional network, becoming a part of a brotherhood, giving back to the community, and becoming a better engineer.
-            <br></br> <br></br>
-            Come and find out what we're all about! We are looking for motivated and bright individuals to become part of our Upsilon Class!
-            <br></br> <br></br>
-            Please fill out the interest form if you'd like to join us for our Spring Rush 2023!
-            </p>
-            <div class = "rush-bttn">
-            <a href="https://forms.gle/K8tom1dVxWma7MA77" target="_blank" rel="noopener noreferrer">
-                <button>Interest Form</button>
-            </a>
+            <div class = "rush-floating1">
+                RUSH RUSH RUSH
             </div>
-            <div class = "pic">
-                <img src={s23} class="pic"></img>
+            <div class = "rush-floating2">
+                RUSH RUSH RUSH
+            </div>
+            <div class = "rush-text">
+                <h1>Are You An Engineer?</h1>
+                <br></br>
+                <p>
+                Come rush for Theta Tau! We're the only Co-ed Professional Engineering Fraternity at UCR. Theta Tau can help you in expanding your professional network, becoming a part of a brotherhood, giving back to the community, and becoming a better engineer.
+                <br></br> <br></br>
+                Come and find out what we're all about! We are looking for motivated and bright individuals to become part of our Upsilon Class!
+                <br></br> <br></br>
+                Please fill out the interest form if you'd like to join us for our Spring Rush 2023!
+                </p>
+                <div class = "rush-buttons">
+                    <a href="https://forms.gle/K8tom1dVxWma7MA77" target = "_blank" rel = "noopener noreferrer">
+                        <button class = "rush-bttn"><span>Interest Form</span></button>
+                    </a>
+                    <a href="https://discord.gg/fC4up6cbSS" target = "_blank" rel = "noopener noreferrer">
+                        <button class = "rush-bttn"><span>Discord Link</span></button>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <img src={s23} class="rush-pic"></img>
             </div>
         </div>
         
