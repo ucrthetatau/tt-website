4e16e910c16196e2254fc8ec0a1f40925e89f122
jerry-nagata <jared.nagata@gmail.com>
2023-08-31 13:56:35 -0700

    updating about page to allow for more content

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ const About = () => {
         <>
         <div class = "about-container">
             <div class = "about-floating1">
-                ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT
+                ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT
             </div>
             <div class = "about-floating2">
-                ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT
+                ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT
             </div>
             <div>
                 <img src={s23} class="about-pic"></img>
@@ -21,11 +21,22 @@ const About = () => {
             <div class = "pic-info">
                 Winter 2023
             </div>
+            
+            <br></br><br></br>
             <div class = "about-text">
                 <p>Theta Tau was originally founded as the "Society of Hammer and Tongs" on October 15, 1904 at the University of Minnesota by four engineers: Erich J. Schrader, Elwin L. Vinal, William M. Lewis, and Isaac B. Hanks. At the fraternity's first national convention in 1911, the name was changed to Theta Tau, and the fraternity opened up to include all departments of engineering. Since then, approximately 30,000 engineering students have been initiated.</p>
-                <h2>Mission Statement</h2>
-                <p>The purpose of Theta Tau is to develop and maintain a high standard of professional interest among its members and to unite them in a strong bond of fraternal fellowship.</p>
+                <br></br>
+                <hr></hr>
+                <br></br><br></br>
+                <div class = "mission-statement">
+                    <h2>Mission Statement</h2>
+                    <p>The purpose of Theta Tau is to develop and maintain a high standard of professional interest among its members and to unite them in a strong bond of fraternal fellowship.</p>
+                </div>
+                <br></br><br></br>
+                <hr></hr>
+                
             </div>
+            <br></br><br></br><br></br><br></br>
         </div>
         </>
     );
