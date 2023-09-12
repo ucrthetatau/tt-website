598f17eb0ab67ac8d442dd08b40416d1de9b5e89
brandontaa <brandonta888@gmail.com>
2023-09-12 14:22:50 -0700

    dropdown edits

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home'
 import Members from './components/Members'
 import About from './components/About'
 import Rush from './components/Rush'
+import Officers from './components/Officers'
 
 
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/members" element={<Members />} />
           <Route path="/about" element={<About />} />
           <Route path="/rush" element={<Rush />} />
+          <Route path="/officers" element={<Officers />} />
         </Routes>
       </div>
     </>
