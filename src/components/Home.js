0cc755adbf9bfe02eb4889e8a209f73a16c56a4b
Davis Kim <daveykim2011@hotmail.com>
2023-09-26 15:19:01 -0700

    Final touches for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,80 +3,63 @@ import "../styles/home.css"
 import brotherhood from "../static/brotherhood.png"
 import professionalism from "../static/professionalism.png"
 import service from "../static/service.png"
-import homePage from "../static/homePage.JPG"
-import { NavLink } from "react-router-dom"
+import { ImageSlider } from "./ImageSlider.js"
 
 const Home = () => {
   return (
     <>
       <div className="page-container">
-        <div className="pic">
-          <div className="center-text-container">
-            <NavLink
-              to="/About"
-              className="nav-link"
-            >
-              <div className="linked-text">About</div>
-            </NavLink>
-          </div>
-          <img src={homePage} alt="homePage"></img>
+        <div className="title-container">
+          <div className="left-line-with-triangle"></div>
+          <div className="title">Chapter</div>
+          <div className="right-line-with-triangle"></div>
+        </div>
+        <div className="home-slider-container">
+          <ImageSlider />
         </div>
         <div className="title-container">
-          <div className="left-line"></div>
-          <div className="title">Our Pillars</div>
-          <div className="right-line"></div>
+          <div className="left-line-with-triangle"></div>
+          <div className="title">Pillars</div>
+          <div className="right-line-with-triangle"></div>
         </div>
-        <div className="home">
-          <div className="container">
-            <div className="pillar">
-              <div className="icon">
-                <img
-                  src={brotherhood}
-                  alt="BrotherhoodIcon"
-                  className="icon"
-                ></img>
-              </div>
-              <div className="subtitle">Brotherhood</div>
-              <div className="border-with-diamonds"></div>
-              <div className="caption">
-                We develop lifelong bonds of fraternal fellowship between our
-                members through unique and memorable experiences. We stay
-                supportive and honest with each other to promote a brotherhood
-                that goes beyond school and work.
-              </div>
-              <div className="border-with-diamonds"></div>
+        <div className="total-pillar-container">
+          <div className="pillar">
+            <img src={brotherhood} alt="BrotherhoodIcon" className="icon"></img>
+            <div className="subtitle">Brotherhood</div>
+            <div className="border-with-diamonds"></div>
+            <div className="caption">
+              We develop lifelong bonds of fraternal fellowship between our
+              members through unique and memorable experiences. We stay
+              supportive and honest with each other to promote a brotherhood
+              that goes beyond school and work.
             </div>
-            <div className="pillar">
-              <div className="icon">
-                <img
-                  src={professionalism}
-                  alt="ProfessionalismIcon="
-                  className="icon"
-                ></img>
-              </div>
-              <div className="subtitle">Professionalism</div>
-              <div className="border-with-diamonds"></div>
-              <div className="caption">
-                We aim to build and refine the professional abilities of our
-                members so that they succeed in their careers and endeavors. We
-                emphasize professional characteristics such as communication,
-                presentation, and innovation.
-              </div>
-              <div className="border-with-diamonds"></div>
+          </div>
+          <div className="pillar">
+            <div className="icon-container">
+              <img
+                src={professionalism}
+                alt="ProfessionalismIcon="
+                className="icon"
+              ></img>
             </div>
-            <div className="pillar">
-              <div className="icon">
-                <img src={service} alt="ServiceIcon" className="icon"></img>
-              </div>
-              <div className="subtitle">Service</div>
-              <div className="border-with-diamonds"></div>
-              <div className="caption">
-                We believe that it is important to excel in our respective
-                fields as well as give back to the community. Our members strive
-                to lead by example and stay true through volunteering, work, and
-                contribution to the school and community.
-              </div>
-              <div className="border-with-diamonds"></div>
+            <div className="subtitle">Professionalism</div>
+            <div className="border-with-diamonds"></div>
+            <div className="caption">
+              We aim to build and refine the professional abilities of our
+              members so that they succeed in their careers and endeavors. We
+              emphasize professional characteristics such as communication,
+              presentation, and innovation.
+            </div>
+          </div>
+          <div className="pillar">
+            <img src={service} alt="ServiceIcon" className="icon"></img>
+            <div className="subtitle">Service</div>
+            <div className="border-with-diamonds"></div>
+            <div className="caption">
+              We believe that it is important to excel in our respective fields
+              as well as give back to the community. Our members strive to lead
+              by example and stay true through volunteering, work, and
+              contribution to the school and community.
             </div>
           </div>
         </div>
@@ -84,12 +67,5 @@ const Home = () => {
     </>
   )
 }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var linkedText = document.querySelector(".linked-text")
-//   linkedText.addEventListener("click", function () {
-//     window.location.href = "/About"
-//   })
-// })
 
 export default Home
