962160db3f1a2fffbf7c9c72c17aaf9d5ecdb918
shreyasjd777 <shreyasjd777@gmail.com>
2023-09-26 18:40:57 -0700

    brothers page under construction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import React from "react"
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -7,7 +8,7 @@ import About from './components/About'
 import Rush from './components/Rush'
 import Officers from './components/Officers'
 import Footer from './components/Footer'
-
+import Brothers from './components/Brothers'
 
 function App() {
   return (
@@ -16,18 +17,21 @@ function App() {
         <Navbar />
         <div class="content-container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/members" element={<Members />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/rush" element={<Rush />} />
-            <Route path="/officers" element={<Officers />} />
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="rush" element={<Rush />} />
+            <Route path="brothers" element={<Brothers />}>
+              {/* <Route index element={<Members />} />
+              <Route path="members" element={<Members />} />
+              <Route path="officers" element={<Officers />} /> */}
+            </Route>
           </Routes>
         </div>
         <div class="footer--pin">
           <Footer />
         </div>
       </div>
-        
     </>
   );
 }
