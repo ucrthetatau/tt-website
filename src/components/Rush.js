425bbf8163cfd786ebb6c588fc168b0dd1fb960b
Davis Kim <daveykim2011@hotmail.com>
2024-04-11 15:03:35 -0700

    Updates to Rush

--- src/components/Rush.js
+++ src/components/Rush.js
@@ -2,61 +2,65 @@ import React from "react"
 // import s23 from "../static/S23graphic1.jpg"
 import blackGear from "../static/blackGear.png"
 import "../styles/rush.css"
+import s24graphic from "../static/Rush BannerWebsite Graphic.png"
 
 function Rush() {
-    return (
-        <>
-        <br></br><br></br><br></br>
-        <div class = "rush-container">
-            {/* <div class = "rush-floating1">
-                RUSH RUSH RUSH
+  return (
+    <>
+      <div class="master">
+        <br></br>
+        <br></br>
+        <br></br>
+        <div class="rush-container">
+          <div class="rush-text">
+            <h1>Are You An Engineer?</h1>
+            <br></br>
+            <p>
+              We are the only co-ed professional engineering fraternity at UCR.
+              Theta Tau can help you in expanding your professional network,
+              becoming a part of a brotherhood, giving back to the community,
+              and developing into a better engineer.
+              <br></br> <br></br>
+              Come and find out what we're all about! We are looking for
+              motivated and bright individuals to become part of our next class in Fall 2024!
+              <br></br> <br></br>
+              {/* Please fill out the interest form if you'd like to join us for our
+              next rush in Spring 2024. Come rush for Theta Tau! */}
+            </p>
+
+            {/* <div class="rush-buttons">
+              <a
+                href="https://forms.gle/WxQ7Dxa5u45CvBqRA"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button class="rush-bttn">
+                  <span>Interest Form</span>
+                </button>
+              </a>
+              <a
+                href="https://discord.gg/EB7jACxzhd"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button class="rush-bttn">
+                  <span>Discord Link</span>
+                </button>
+              </a>
             </div> */}
-        {/* <div class = "rush-floating2">
-                RUSH RUSH RUSH
-            </div> */}
-        <div class="rush-text">
-          <h1>Are You An Engineer?</h1>
-          <br></br>
-          <p>
-            We are the only co-ed professional engineering fraternity at UCR. Theta Tau can help you in expanding
-            your professional network, becoming a part of a brotherhood, giving back to the community, and developing into a better engineer.
-            <br></br> <br></br>
-            Come and find out what we're all about! We are looking for motivated
-            and bright individuals to become part of our next class!
-            <br></br> <br></br>
-            {/* Please fill out the interest form if you'd like to join us for our
-            next rush in Spring 2024. Come rush for Theta Tau!  */}
-            Please follow our instagram to keep up to date with our next rush in Spring 2024!
-          </p>
-          {/* <div class="rush-buttons">
-            <a
-              href="https://forms.gle/K8tom1dVxWma7MA77"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button class="rush-bttn">
-                <span>Interest Form</span>
-              </button>
-            </a>
-            <a
-              href="https://discord.gg/fC4up6cbSS"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button class="rush-bttn">
-                <span>Discord Link</span>
-              </button>
-            </a>
+          </div>
+
+          {/* <div>
+            <img src={s24graphic} class="rush-pic"></img>
+          </div> */}
+          {/* <div>
+            <img src={blackGear} class="black gear"></img>
           </div> */}
         </div>
-        {/* <div>
-          <img src={s23} class="rush-pic"></img>
-        </div> */}
-        <div>
-          <img src={blackGear} class="black gear"></img>
-        </div>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
-      <br></br><br></br><br></br>
     </>
   )
 }
