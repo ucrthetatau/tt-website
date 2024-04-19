b0da701255832d4a184490f20809a81d4facd01d
shreyasjd777 <shreyasjd777@gmail.com>
2024-04-19 15:00:03 -0700

    created brother card

---
+++ src/components/BrotherCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import "../styles/brothercard.css";
+import { brothers, officers, classes, classMap, yearMap } from './QueryDB.js'
+import { useState, useEffect } from 'react';
+
+const BrotherCard = ({ brotherId }) => {
+    const name = brothers[brotherId]["Name"]
+    const greek = brothers[brotherId]["Class"]
+    const major = brothers[brotherId]["Major"]
+    const year = brothers[brotherId]["GraduatingClass"]
+    const pic = brothers[brotherId]["Photo"]
+
+    // function flipCard(event) {
+    //     event.target.classList.toggle("flipCard");
+    // }
+
+    // const temp = document.querySelector(".flip")
+    // console.log(temp)
+    // console.log("hi")
+
+    // temp.addEventListener("click", () => {
+    //     temp.classList
+    // });
+
+    return (
+        <div class="brotherCard" >
+            <div class="flip">
+                <div class="front" >
+                    <img class="headshot" src={pic}/>
+                </div>
+                <div class="back">
+                    <div class="details">
+                        <h2>Class</h2>
+                        <h3>{greek}</h3>
+                        <h2>Major</h2>
+                        <h3>{major}</h3>
+                        <h2>Year</h2>
+                        <h3>{year}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="info">
+                <h1>{name}</h1>
+            </div>
+        </div>
+    )
+};
+
+export default BrotherCard;
--- src/components/Brothers.js
+++ src/components/Brothers.js
@@ -3,6 +3,7 @@ import "../styles/brothers.css";
 // import { NavLink, Outlet } from 'react-router-dom';
 import { brothers, officers, classes, classMap, yearMap } from './QueryDB.js'
 import { useState, useEffect } from 'react';
+import Footer from "./Footer.js";
 
 // console.log(brothers)
 // console.log(officers)
@@ -10,13 +11,62 @@ import { useState, useEffect } from 'react';
 // console.log(classMap)
 // console.log(yearMap)
 // console.log("hi")
+import BrotherCard from "./BrotherCard.js"
 
 const Brothers = () => {
+
+    const [pageFilter, setPageFilter] = useState("Class")
+
+    function changeFilter(event) {
+        const filters = document.querySelectorAll(".filter");
+        filters.forEach((filter) => {
+          filter.classList.remove("active");
+        });
+        event.target.classList.add("active");
+        setPageFilter(event.target.innerHTML);
+    }
         return (
-            <>
-                 
-            </>
-        )
+            <div class="brothers -mt-20 h-full w-full">
+                {/* <div className="banner h-screen w-screen flex justify-center items-center bg-cover bg-center bg-fixed">
+                    <div className="bannerText absolute text-center">
+                    <h1>OUR BROTHERS</h1>
+                    <h3>of the Epsilon Delta Chapter</h3>
+                    </div>
+                </div> */}
+
+                <div class="content w-full">
+                    <div class="filters flex flex-row items-center justify-center">
+                        <h1 class="active filter text-center font-extrabold cursor-pointer" onClick={changeFilter}>
+                            Class
+                        </h1>
+                        <h1 class="filter text-center font-extrabold cursor-pointer" onClick={changeFilter}>
+                            Officers
+                        </h1>
+                        <h1 class="filter text-center font-extrabold cursor-pointer" onClick={changeFilter}>
+                            Name
+                        </h1>
+                        <h1 class="filter text-center font-extrabold cursor-pointer" onClick={changeFilter}>
+                            Year
+                        </h1>
+                    </div>
+
+                    <div class={`w-full flex flex-col items-center justify-center ${pageFilter === "Class" ? "" : "hidden"}`} id="class">
+                        {/* <BrotherFilter category="Class"></BrotherFilter> */}
+                    </div>
+                    <div class={`w-full flex flex-col items-center justify-center ${pageFilter === "Officers" ? "" : "hidden"}`} id="officers">
+                        {/* <BrotherFilter category="Officers"></BrotherFilter> */}
+                    </div>
+                    <div class={`w-full flex flex-col items-center justify-center ${pageFilter === "Name" ? "" : "hidden"}`} id="name">
+                        {/* <BrotherFilter category="Name"></BrotherFilter> */}
+                    </div>
+                    <div class={`w-full flex flex-col items-center justify-center ${pageFilter === "Year" ? "" : "hidden"}`} id="year">
+                        {/* <BrotherFilter category="Year"></BrotherFilter> */}
+                    </div> 
+                </div>
+
+                <BrotherCard brotherId="shreyasDeshpande"></BrotherCard>
+            </div>
+        );
         // <div id="container">
         //     {classes.map(currentClass => (
         //         <div class="group">
