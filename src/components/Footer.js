05a8b1bfba1a96301f59b4ac8cf2143fca4749eb
shreyasjd777 <shreyasjd777@gmail.com>
2024-04-21 02:35:20 -0700

    added alert popout for credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,32 @@ import '../styles/footer.css'
 import Letters from '../static/Letters.png'
 import Logo from '../static/Logo.PNG'
 
-const Footer = () => (
-    <div class="footer">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
-        <div class="footer-contents">
-            <div class="buttons">
-                <a href="https://thetatau.org">
-                    <img id="letters" src={Letters}/>
-                </a>
-                <a id="insta" href='https://www.instagram.com/thetatauucr/'>
-                    <i  class="bi bi-instagram"></i>
-                </a>
-                <a href="mailto:thetatauucr@gmail.com">
-                    <i id="mail" class="bi bi-envelope"></i>
-                </a>
+const Footer = () => {
+    
+    function showCredits() {
+        alert("Contributors:\n\nShreyas Deshpande\nJared Nagata\nDavis Kim\nTerence Mui\nNuha Rahman\nCatherine Truong\nBrandon Ta\nNanma Pillai")
+    }
+
+    return (
+        <div class="footer">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
+            <div class="footer-contents">
+                <div class="buttons">
+                    <a href="https://thetatau.org">
+                        <img id="letters" src={Letters}/>
+                    </a>
+                    <a id="insta" href='https://www.instagram.com/thetatauucr/'>
+                        <i  class="bi bi-instagram"></i>
+                    </a>
+                    <a href="mailto:thetatauucr@gmail.com">
+                        <i id="mail" class="bi bi-envelope"></i>
+                    </a>
+                </div>
+                <p class="credits" onClick={showCredits}>Website Credits</p>
             </div>
-            <p class="credits">Website Credits</p>
-        </div>
-    </div>      
-);
+        </div>   
+    )   
+};
 
         {/* <div class="column" id="col1">
             <img src={Logo} class="crest"/>
