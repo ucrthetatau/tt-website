0bed893e785d813a694a9cc48c85dcfa9b04e10f
shreyasjd777 <shreyasjd777@gmail.com>
2025-03-25 02:36:02 -0700

    finished navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,35 +3,42 @@ import styles from '../styles/navbar.module.css'
 import NavLogo from '../static/NavLogo.png'
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-    <header className={styles.header}>
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>  
-        
-        <NavLink to="/" className={styles.main}>
-            <img src={NavLogo} alt="Theta Tau Crest" className={styles.navLogo}/>
-        </NavLink>
+function Navbar() {
+    
+    const closeMenu = () => {
+        document.getElementById('check').checked = false;
+    };
 
-        <input type='checkbox' id="check" className={styles.menuCheck}/>
-        <label htmlFor='check' className={styles.icons}>
-            <i className='bx bx-menu' id="open"></i>
-            <i className='bx bx-x' id="close"></i>
-        </label> 
+    return (
+        <header className={styles.header}>
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>  
+            
+            <NavLink to="/" className={styles.main} onClick={closeMenu}>
+                <img src={NavLogo} alt="Theta Tau Crest" className={styles.navLogo}/>
+            </NavLink>
 
-        <div className={styles.nav}>
-            <NavLink to="/" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link}> 
-                Home
-            </NavLink>
-            <NavLink to="/about" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link}>
-                About
-            </NavLink>
-            <NavLink to="/rush" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link}>
-                Rush
-            </NavLink>
-            <NavLink to="/brothers" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link}>
-                Brothers
-            </NavLink>
-        </div>
-    </header>
-);
+            <input type='checkbox' className={styles.toggle} id="check"/>
+            <label htmlFor='check' className={styles.icons}>
+                <i className={`bx bx-menu ${styles.open}`}></i>
+                <i className={`bx bx-x ${styles.close}`}></i>
+            </label> 
+
+            <div className={styles.nav}>
+                <NavLink to="/" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link} onClick={closeMenu}> 
+                    Home
+                </NavLink>
+                <NavLink to="/about" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link} onClick={closeMenu}>
+                    About
+                </NavLink>
+                <NavLink to="/rush" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link} onClick={closeMenu}>
+                    Rush
+                </NavLink>
+                <NavLink to="/brothers" className={({isActive}) => isActive ? `${styles.link} ${styles.active}` : styles.link} onClick={closeMenu}>
+                    Brothers
+                </NavLink>
+            </div>
+        </header>
+    );
+};
 
 export default Navbar;
